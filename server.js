8738ced139762596a835d05ff5dd8c783e804ed4
muttinthehut <joeb615@gmail.com>
2014-09-17 03:09:26 +0000

    Update all

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var util = require('util'),
     connect = require('connect'),
     serveStatic = require('serve-static'),
-    port = process.env.PORT;
+    port = process.env.PORT || 3000;
 connect().use(serveStatic(__dirname)).listen(port);
 
